370f1ea70120b800d7abf454926763841c914086
Stefan Eischet <stefan@eischet.net>
2024-11-20 16:16:31 +0100

    Sample code

---
+++ src/main/java/com/example/notnulltest/TestClass.java
@@ -0,0 +1,27 @@
+package com.example.notnulltest;
+
+import org.jetbrains.annotations.NotNull;
+
+public class TestClass {
+
+    public static void foo() {
+        final @NotNull String bar = "bar";
+        var baz = new Baz(bar);
+
+        /* Alt-Enter on Baz and then selecting "Create Class 'Baz'" generates the following code:
+
+        package com.example.notnulltest;
+
+        import org.jetbrains.annotations.NotNull;
+
+        public class Baz {
+            public Baz(final @NotNull @NotNull String bar) {
+            }
+        }
+
+        Which cannot compile, because you cannot stack @NotNull.
+
+         */
+    }
+
+}
